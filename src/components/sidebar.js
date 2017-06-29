bb235545ef04f66d1789320086c560e36ff76768
Ruoni Wang <Ruoni.wang.19@Dartmouth.edu>
2017-06-29 01:20:43 -0400

    projects displayed in nav bar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,46 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { SideNav, SideNavItem } from 'react-materialize';
-import axios from 'axios';
 
-const ROOT_URL = 'localhost:9090/dali';
-class SideBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projects: ['Pulse', 'Grafficity', 'Seabird'],
-    };
-  }
-  // fill up the projects data
-  ComponentDidMount() {
-    axios.get(`${ROOT_URL}/projects`).then((response) => {
-      this.setState({ projects: response });
-    });
-  }
-  render() {
-    return (
-      <SideNav
-        trigger={<i className="material-icons">toc</i>}
-        options={{ closeOnClick: false }}
-      >
-        <SideNavItem userView
-          user={{
-            background: 'images/dali-logo.jpg',
-            name: 'DALI',
-          }}
-        />
+const SideBar = (props) => {
+  return (
+    <SideNav
+      trigger={<i className="material-icons">toc</i>}
+      options={{ closeOnClick: false }}
+    >
+      <SideNavItem userView
+        user={{
+          background: 'images/dali-logo.jpg',
+          name: 'DALI',
+        }}
+      />
 
-        <SideNavItem divider />
-        <SideNavItem subheader>Projects</SideNavItem>
-        {this.state.projects.entrySeq().map(([id, name]) => {
-          const link = `project/${id}`;
-          return (
-            <SideNavItem href={link}>{name}</SideNavItem>
-          );
-        })}
+      <SideNavItem divider />
+      <SideNavItem subheader>Projects</SideNavItem>
+      {props.projects.entrySeq().map(([name, id]) => {
+        const link = `project/${id}`;
+        console.log('loading!');
+        return (
+          <SideNavItem href={link}>{name}</SideNavItem>
+        );
+      })}
 
-      </SideNav>
+    </SideNav>
 
-    );
-  }
-}
+  );
+};
+
 export default SideBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Immutable from 'immutable';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './style.scss';
 import SideBar from './components/sidebar';
 import Project from './components/project';
 import People from './components/people';
 import Profile from './components/profile';
+
+const ROOT_URL = 'http://localhost:9090/api';
 
 const Welcome = (props) => {
   return (
@@ -22,23 +26,42 @@ const FallBack = (props) => {
     </div>
   );
 };
-const App = (props) => {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: Immutable.Map(),
+    };
+  }
+  componentDidMount() {
+    console.log('mounting!');
+    axios.get(`${ROOT_URL}/projects`).then((responses) => {
+      responses.data.forEach((response) => {
+        this.setState({ projects: this.state.projects.set(response.name, response._id) });
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <SideBar projects={this.state.projects} />
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/project/:id" component={Project} />
+            <Route path="/people" component={People} />
+            <Route path="/member" component={Profile} />
+            <Route component={FallBack} />
+          </Switch>
+        </div>
+      </Router>
 
-    <Router>
-      <div>
-        <SideBar />
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/project/:id" component={Project} />
-        <Route path="/people" component={People} />
-        <Route path="/member" component={Profile} />
-        <Route component={FallBack} />
-      </div>
-    </Router>
 
-
-  );
-};
+    );
+  }
+}
 
 
 ReactDOM.render(<App />, document.getElementById('main'));
